0feea8d040c3b9b044fde1b15d08883ea7b25043
Netherfoam <i.got.burnt@gmail.com>
2012-07-22 17:07:51 +1000

    Remove display item if we find an invalid shop

--- src/org/maxgamer/QuickShop/Watcher/ItemWatcher.java
+++ src/org/maxgamer/QuickShop/Watcher/ItemWatcher.java
@@ -21,7 +21,7 @@ public class ItemWatcher implements Runnable{
 		QuickShop plugin = (QuickShop) Bukkit.getServer().getPluginManager().getPlugin("QuickShop");
 		HashSet<Location> toRemove = new HashSet<Location>(5);
 		for(Entry<Location, Shop> entry : plugin.getShops().entrySet()){
-			if(entry.getValue().getLocation().getWorld() == null) continue;
+			if(entry.getValue().getLocation().getWorld() == null) continue; //World not loaded
 			
 			DisplayItem disItem = entry.getValue().getDisplayItem();
 			if(entry.getValue().getLocation().getBlock() != null && entry.getValue().getLocation().getBlock().getType() != Material.CHEST){
@@ -34,14 +34,18 @@ public class ItemWatcher implements Runnable{
 				
 				plugin.getLogger().info("Shop is not a chest in " +world + " at: " + x + ", " + y + ", " + z + ".  Removing from DB.");
 				plugin.getDB().writeToBuffer("DELETE FROM shops WHERE x = "+x+" AND y = "+y+" AND z = "+z+" AND world = '"+world+"'");
-				//We can't remove it yet, we're still iterating!
+				
+				//We can't remove it yet, we're still iterating over this!
 				toRemove.add(loc);
+				//But we can delete the shops display item
+				entry.getValue().getDisplayItem().remove();
 			}
 			else if(entry.getKey().getChunk().isLoaded() && disItem.getItem().getTicksLived() >= 5000 || disItem.getItem().isDead() || disItem.getDisplayLocation().distanceSquared(disItem.getItem().getLocation()) > 1){
 				disItem.removeDupe();
 				disItem.respawn();
 			}
 		}
+		//Now we can remove it.
 		for(Location loc : toRemove){
 			plugin.getShops().remove(loc);
 		}
