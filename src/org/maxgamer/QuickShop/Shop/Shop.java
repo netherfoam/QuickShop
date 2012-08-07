91ed355fc2be914dae7e62dc1dd5786552e3e378
Netherfoam <i.got.burnt@gmail.com>
2012-08-07 15:22:19 +1000

    Added GetRemaingSpace() Added matches() Added sell() Added buy()

--- src/org/maxgamer/QuickShop/Shop/Shop.java
+++ src/org/maxgamer/QuickShop/Shop/Shop.java
@@ -1,12 +1,16 @@
 package org.maxgamer.QuickShop.Shop;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
+import org.bukkit.block.Sign;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.maxgamer.QuickShop.QuickShop;
@@ -18,6 +22,7 @@ public class Shop{
 	private ItemStack item;
 	private DisplayItem displayItem;
 	private boolean unlimited;
+	private ShopType shopType;
 	
 	private QuickShop plugin;
 	
@@ -36,6 +41,8 @@ public class Shop{
 		this.plugin = (QuickShop) Bukkit.getPluginManager().getPlugin("QuickShop");
 		this.item.setAmount(1);
 		this.displayItem = new DisplayItem(plugin, this, this.item);
+		
+		this.shopType = ShopType.SELLING;
 	}
 	/**
 	 * Returns the number of items this shop has in stock.
@@ -49,13 +56,46 @@ public class Shop{
 		
 		ItemStack[] in = chest.getInventory().getContents();
 		for(ItemStack item : in){
-			if(item != null && item.getType() == getMaterial() && item.getDurability() == getDurability() && item.getEnchantments().equals(getEnchants())){
+			if(this.matches(item)){
 				stock = stock + item.getAmount();
 			}
 		}
 		
 		return stock;
 	}
+	
+	/**
+	 * Returns the number of free spots in the chest for the particular item.
+	 * @param stackSize
+	 * @return
+	 */
+	public int getRemainingSpace(int stackSize){
+		if(this.unlimited) return 9999;
+		
+		Chest chest = (Chest) loc.getBlock().getState();
+		int space = 0;
+		
+		ItemStack[] inv = chest.getInventory().getContents();
+		for(ItemStack item : inv){
+			if(item == null || item.getType() == Material.AIR || item.getAmount() == 0){
+				space += stackSize;
+			}
+			else if(this.matches(item)){
+				space += stackSize - item.getAmount();
+			}
+		}
+		
+		return space;
+	}
+	/**
+	 * Returns true if the ItemStack matches what this shop is selling/buying
+	 * @param item The ItemStack
+	 * @return True if the ItemStack is the same (Excludes amounts)
+	 */
+	public boolean matches(ItemStack item){
+		return (item != null && item.getType() == getMaterial() && item.getDurability() == getDurability() && item.getEnchantments().equals(getEnchants()));
+	}
+	
 	/**
 	 * @return The location of the shops chest
 	 */
@@ -74,6 +114,38 @@ public class Shop{
 	 */
 	public Material getMaterial(){
 		return this.item.getType();
+	}
+	
+	/**
+	 * Updates the shop in the database.
+	 * @param isNew True if this shop is not in the database yet
+	 */
+	public void update(boolean isNew){
+		int x = this.getLocation().getBlockX();
+		int y = this.getLocation().getBlockY();
+		int z = this.getLocation().getBlockZ();
+		
+		String world = this.getLocation().getWorld().getName();
+		
+		int unlimited = this.isUnlimited() ? 1 : 0;
+		int type = this.getShopType()==ShopType.SELLING ? 1 : 0;
+
+		String q = "";
+		if(isNew){
+			q = "INSERT INTO shops VALUES ('"+this.getOwner()+"', '"+this.getPrice()+"', '"+plugin.makeString(this.item)+"', '"+x+"', '"+y+"', '"+z+"', '"+world+"', '"+unlimited+"', '"+type+"')";
+		}
+		else{
+			q = "UPDATE shops SET owner = '"+this.getOwner()+"', itemString = '"+plugin.makeString(this.item)+"', unlimited = '"+unlimited+"', type = '"+type+"'  WHERE x = '"+x+"' AND y = '"+y+"' AND z = '"+z+"' AND world = '"+world+"'";  
+		}
+		
+		plugin.getDB().writeToBuffer(q);
+	}
+	
+	/**
+	 * Upates the shop into the database.
+	 */
+	public void update(){
+		this.update(false);
 	}
 	
 	/**
@@ -125,6 +197,94 @@ public class Shop{
 		}
 	}
 	
+	/**
+	 * Add an item from to shop.
+	 * @param item The itemstack.  The amount does not matter, just everything else
+	 * @param amount The amount to add to the shop.
+	 */
+	public void add(ItemStack item, int amount){
+		if(this.unlimited) return;
+		
+		Inventory inv = this.getChest().getInventory();
+		
+		int remains = amount;
+		
+		while(remains > 0){
+			int stackSize = Math.min(remains, item.getMaxStackSize());
+			item.setAmount(stackSize);
+			inv.addItem(item);
+			remains = remains - stackSize;
+		}
+	}
+	
+	/**
+	 * Sells amount of item to Player p.  Does NOT check our inventory, or balances
+	 * @param p The player to sell to
+	 * @param item The itemStack to sell
+	 * @param amount The amount to sell
+	 */
+	public void sell(Player p, ItemStack item, int amount){
+		if(amount < 0) this.buy(p, item, -amount);
+		//Items to drop on floor
+		HashMap<Integer, ItemStack> floor = new HashMap<Integer, ItemStack>(30);
+		//We do NOT want to modify this
+		ItemStack transfer = item.clone();
+		
+		if(!this.isUnlimited()){
+			this.remove(transfer, amount);
+		}
+		
+		while(amount > 0){
+			int stackSize = Math.min(amount, transfer.getMaxStackSize());
+			if(stackSize == -1){
+				stackSize = amount;
+			}
+			
+			transfer.setAmount(stackSize);
+			
+			//Give the player the items.
+			//Store the leftover items they didn't have room for
+			floor.putAll(p.getInventory().addItem(transfer));
+			amount -= stackSize;
+		}
+		
+		//Drop the remainder on the floor.
+		for(int i = 0; i < floor.size(); i++){
+			p.getWorld().dropItem(p.getLocation(), floor.get(i));								
+		}
+	}
+	
+	/**
+	 * Buys amount of item from Player p.  Does NOT check our inventory, or balances
+	 * @param p The player to buy from
+	 * @param item The itemStack to buy
+	 * @param amount The amount to buy
+	 */
+	public void buy(Player p, ItemStack item, int amount){
+		if(amount < 0) this.sell(p, item, -amount);
+		
+		//We do NOT want to modify this
+		ItemStack transfer = item.clone();
+		if(!this.isUnlimited()){
+			this.add(transfer, amount);
+		}
+		
+		while(amount > 0){
+			int stackSize = Math.min(amount, transfer.getMaxStackSize());
+			if(stackSize == -1){
+				stackSize = amount;
+			}
+			
+			transfer.setAmount(stackSize);
+			
+			//Give the player the items.
+			//Store the leftover items they didn't have room for
+			this.getChest().getInventory().addItem(transfer);
+			
+			amount -= stackSize;
+		}
+	}
+	
 	public void setOwner(String owner){
 		this.owner = owner;
 	}
@@ -142,5 +302,105 @@ public class Shop{
 	}
 	public boolean isUnlimited(){
 		return this.unlimited;
+	}
+	
+	public enum ShopType{
+		SELLING(),
+		BUYING();
+	}
+	
+	public ShopType getShopType(){
+		return this.shopType;
+	}
+	
+	public boolean isBuying(){
+		return this.shopType == ShopType.BUYING;
+	}
+	
+	public boolean isSelling(){
+		return this.shopType == ShopType.SELLING;
+	}
+	
+	/**
+	 * Changes a shop type to Buying or Selling. Also updates the signs nearby
+	 * @param shopType The new type (ShopType.BUYING or ShopType.SELLING)
+	 */
+	public void setShopType(ShopType shopType){
+		this.shopType = shopType;
+		
+		String[] lines = new String[3];
+		if(this.isBuying()){
+			lines[0] = "Buying:";
+		}
+		if(this.isSelling()){
+			lines[0] = "Selling:";
+		}
+		lines[1] = plugin.getDataName(this.getMaterial(), this.getDurability());
+		lines[2] = "For " + this.price + " each";
+		this.setSignText(lines);
+	}
+	
+	/**
+	 * Changes all lines of text on a sign near the shop
+	 * @param lines The array of lines to change. Index is line number.
+	 */
+	public void setSignText(String[] lines){
+		Block[] blocks = new Block[4];
+		blocks[0] = loc.getBlock().getRelative(1, 0, 0);
+		blocks[1] = loc.getBlock().getRelative(-1, 0, 0);
+		blocks[2] = loc.getBlock().getRelative(0, 0, 1);
+		blocks[3] = loc.getBlock().getRelative(0, 0, -1);
+		
+		for(Block b : blocks){
+			if(b.getType() != Material.WALL_SIGN) continue;
+			Sign sign = (Sign) b.getState();
+			
+			for(int i = 0; i < lines.length; i++){
+				sign.setLine(i, lines[i]);
+			}
+			
+			sign.update(true);
+		}
+	}
+	
+	public String getDataName(){
+		return plugin.getDataName(this.getMaterial(), this.getDurability());
+	}
+	
+	/**
+	 * Deletes the shop from the list of shops
+	 * and queues it for database deletion
+	 */
+	public void delete(){
+		//Delete the display item
+		this.getDisplayItem().remove();
+		
+		//Delete the signs around it
+		Block[] blocks = new Block[4];
+		blocks[0] = loc.getBlock().getRelative(1, 0, 0);
+		blocks[1] = loc.getBlock().getRelative(-1, 0, 0);
+		blocks[2] = loc.getBlock().getRelative(0, 0, 1);
+		blocks[3] = loc.getBlock().getRelative(0, 0, -1);
+		
+		for(Block b : blocks){
+			if(b.getType() == Material.WALL_SIGN){
+				b.setType(Material.AIR);
+			}
+		}
+		
+		//Delete it from the database
+		int x = this.getLocation().getBlockX();
+		int y = this.getLocation().getBlockY();
+		int z = this.getLocation().getBlockZ();
+		String world = this.getLocation().getWorld().getName();
+		plugin.getDB().writeToBuffer("DELETE FROM shops WHERE x = '"+x+"' AND y = '"+y+"' AND z = '"+z+"' AND world = '"+world+"'");
+		
+		//Refund if necessary
+		if(plugin.getConfig().getBoolean("shop.refund")){
+			plugin.getEcon().depositPlayer(this.getOwner(), plugin.getConfig().getDouble("shop.cost"));
+		}
+		
+		//Delete it from memory
+		plugin.getShops().remove(this.getLocation());
 	}
 }
