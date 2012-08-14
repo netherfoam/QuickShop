5c9ab313f1a87fe89d1084eb64e085482a4d1532
Netherfoam <i.got.burnt@gmail.com>
2012-08-14 16:33:23 +1000

    Optimise.  Delete duplicate listener. Sneaking optimisation.

--- src/org/maxgamer/QuickShop/Listeners/ClickListener.java
+++ src/org/maxgamer/QuickShop/Listeners/ClickListener.java
@@ -1,5 +1,6 @@
 package org.maxgamer.QuickShop.Listeners;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -13,7 +14,6 @@ import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -56,17 +56,37 @@ public class ClickListener implements Listener{
 	 * Left click a SHOP   chest			: Send purchase menu
 	 */
 	public void onClick(PlayerInteractEvent e){
-		if(e.isCancelled() || e.getAction() != Action.LEFT_CLICK_BLOCK || (e.getClickedBlock().getType() != Material.CHEST && e.getClickedBlock().getType() != Material.WALL_SIGN)) return;
+		if(e.isCancelled()) return;
+		if(e.getClickedBlock().getType() != Material.CHEST && e.getClickedBlock().getType() != Material.WALL_SIGN) return;
 		
-		Block b = e.getClickedBlock();
+		if(e.getAction() == Action.RIGHT_CLICK_BLOCK){
+			if(e.getClickedBlock().getType() == Material.CHEST && plugin.lock){
+				Shop shop = plugin.getShop(e.getClickedBlock().getLocation());
+				if(shop != null && !shop.getOwner().equalsIgnoreCase(e.getPlayer().getName())){
+					if(e.getPlayer().hasPermission("quickshop.other.open")){
+						e.getPlayer().sendMessage(ChatColor.RED + "Bypassing a QuickShop lock!");
+						return;
+					}
+					e.getPlayer().sendMessage(ChatColor.RED + "[QuickShop] That shop is locked.  Left click if you wish to buy!");
+					e.setCancelled(true);
+					return;
+				}
+			}
+		}
+		else if(e.getAction() != Action.LEFT_CLICK_BLOCK){
+			return;
+		}
+		
 		Player p = e.getPlayer();
-		ItemStack item = e.getItem();
-		Location loc = b.getLocation();
 		
-		if(plugin.getConfig().getBoolean("shop.sneak-only") && !p.isSneaking()){
+		if(plugin.sneak && !p.isSneaking()){
 			//Sneak only
 			return;
 		}
+		
+		Block b = e.getClickedBlock();
+		Location loc = b.getLocation();
+		ItemStack item = e.getItem();
 		
 		//Get the shop
 		Shop shop = plugin.getShop(loc);
@@ -97,15 +117,13 @@ public class ClickListener implements Listener{
 		/*
 		 * Creation handling
 		 */
-		else if(item != null && item.getType() != Material.AIR && p.hasPermission("quickshop.create.sell") && b.getType() == Material.CHEST && shop == null){
+		else if(shop == null && item != null && item.getType() != Material.AIR && p.hasPermission("quickshop.create.sell") && b.getType() == Material.CHEST){
 			if(!plugin.canBuildShop(p, b)){
 				p.sendMessage(ChatColor.RED + "You may not create a shop here.");
-				//e.setCancelled(true);
 				return;
 			}
 			if(plugin.getChestNextTo(b) != null){
 				p.sendMessage(ChatColor.RED + "Double chest shops are disabled.");
-				//e.setCancelled(true);
 				return;
 			}
 			
@@ -138,65 +156,54 @@ public class ClickListener implements Listener{
 		sendShopInfo(p, shop, shop.getRemainingStock());
 	}
 	private void sendShopInfo(Player p, Shop shop, int stock){
+		//Potentially faster with an array?
 		ItemStack items = shop.getItem();
-		p.sendMessage("");
-		p.sendMessage("");
-		p.sendMessage(ChatColor.DARK_PURPLE + "+---------------------------------------------------+");
-		p.sendMessage(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Shop Information:");
-		p.sendMessage(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Owner: " + shop.getOwner());
-		p.sendMessage(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Item: " + ChatColor.YELLOW + plugin.getDataName(items.getType(), items.getDurability()));
+		List<String> messages = new ArrayList<String>(15);
+		messages.add("");
+		messages.add("");
+		
+		messages.add(ChatColor.DARK_PURPLE + "+---------------------------------------------------+");
+		messages.add(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Shop Information:");
+		messages.add(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Owner: " + shop.getOwner());
+		messages.add(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Item: " + ChatColor.YELLOW + plugin.getDataName(items.getType(), items.getDurability()));
 		
 		if(shop.isSelling()){
-			p.sendMessage(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Stock: " + ChatColor.YELLOW + stock);
+			messages.add(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Stock: " + ChatColor.YELLOW + stock);
 		}
 		else{
-			p.sendMessage(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Space: " + ChatColor.YELLOW + shop.getRemainingSpace(shop.getMaterial().getMaxStackSize()));
+			messages.add(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Space: " + ChatColor.YELLOW + shop.getRemainingSpace(shop.getMaterial().getMaxStackSize()));
 		}
-		p.sendMessage(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Price per "+ChatColor.YELLOW + items.getType() + ChatColor.GREEN + " - " + ChatColor.YELLOW + plugin.getEcon().format(shop.getPrice()));
-		p.sendMessage(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Total Value of Chest: " + ChatColor.YELLOW + plugin.getEcon().format(shop.getPrice() * stock));
+		messages.add(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Price per "+ChatColor.YELLOW + items.getType() + ChatColor.GREEN + " - " + ChatColor.YELLOW + plugin.getEcon().format(shop.getPrice()));
+		messages.add(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "Total Value of Chest: " + ChatColor.YELLOW + plugin.getEcon().format(shop.getPrice() * stock));
 		
 		if(plugin.isTool(items.getType())){
-			p.sendMessage(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + plugin.getToolPercentage(items) + "% Remaining"); 
+			messages.add(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + plugin.getToolPercentage(items) + "% Remaining"); 
 		}
 		
 		if(shop.isBuying()){
-			p.sendMessage(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "This shop is " + ChatColor.LIGHT_PURPLE + "BUYING" + ChatColor.GREEN + " items.");
+			messages.add(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "This shop is " + ChatColor.LIGHT_PURPLE + "BUYING" + ChatColor.GREEN + " items.");
 		}
 		else{
-			p.sendMessage(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "This shop is " + ChatColor.AQUA + "SELLING" + ChatColor.GREEN + " items.");
+			messages.add(ChatColor.DARK_PURPLE + "| " + ChatColor.GREEN + "This shop is " + ChatColor.AQUA + "SELLING" + ChatColor.GREEN + " items.");
 		}
 			
 		Map<Enchantment, Integer> enchs = items.getEnchantments();
 		if(enchs != null && enchs.size() > 0){
-			p.sendMessage(ChatColor.DARK_PURPLE + "+--------------------ENCHANTS-----------------------+");
+			messages.add(ChatColor.DARK_PURPLE + "+--------------------ENCHANTS-----------------------+");
 			for(Entry<Enchantment, Integer> entries : enchs.entrySet()){
-				p.sendMessage(ChatColor.DARK_PURPLE + "| " + ChatColor.YELLOW + entries.getKey() .getName() + " " + entries.getValue() );
+				messages.add(ChatColor.DARK_PURPLE + "| " + ChatColor.YELLOW + entries.getKey() .getName() + " " + entries.getValue() );
 			}
 		}
-		p.sendMessage(ChatColor.DARK_PURPLE + "+---------------------------------------------------+");
-	}
-
-	@EventHandler(priority = EventPriority.HIGH)
-	/**
-	 * Locks chests if enabled in config.
-	 */
-	public void onChestUse(PlayerInteractEvent e){
-		if(e.isCancelled() || e.getAction() != Action.RIGHT_CLICK_BLOCK || e.getClickedBlock().getType() != Material.CHEST) return;
+		messages.add(ChatColor.DARK_PURPLE + "+---------------------------------------------------+");
 		
-		if(plugin.getConfig().getBoolean("shop.lock")){
-			Shop shop = plugin.getShop(e.getClickedBlock().getLocation());
-			if(shop != null && !shop.getOwner().equalsIgnoreCase(e.getPlayer().getName())){
-				if(e.getPlayer().hasPermission("quickshop.other.open")){
-					e.getPlayer().sendMessage(ChatColor.RED + "Bypassing a QuickShop lock!");
-					return;
-				}
-				e.getPlayer().sendMessage(ChatColor.RED + "[QuickShop] That shop is locked.  Left click if you wish to buy!");
-				e.setCancelled(true);
-				return;
-			}
-		}
+		p.sendMessage((String[]) messages.toArray());
 	}
 	
+	/**
+	 * Gets the shop a sign is attached to
+	 * @param loc The location of the sign
+	 * @return The shop
+	 */
 	private Shop getShopNextTo(Location loc){
 		Block[] blocks = new Block[4];
 		blocks[0] = loc.getBlock().getRelative(1, 0, 0);
