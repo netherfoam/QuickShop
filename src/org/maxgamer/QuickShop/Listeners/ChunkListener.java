351a336d2facebe0d7ab06d9abd5968bf406ca81
Netherfoam <i.got.burnt@gmail.com>
2012-08-15 17:44:53 +1000

    Try improve chunk performance

--- src/org/maxgamer/QuickShop/Listeners/ChunkListener.java
+++ src/org/maxgamer/QuickShop/Listeners/ChunkListener.java
@@ -1,5 +1,9 @@
 package org.maxgamer.QuickShop.Listeners;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.bukkit.Chunk;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -12,35 +16,56 @@ import org.maxgamer.QuickShop.Shop.Shop;
 
 
 public class ChunkListener implements Listener{
-	QuickShop plugin;
+	private QuickShop plugin;
+	public HashMap<Chunk, List<Shop>> chunkMap = new HashMap<Chunk, List<Shop>>(10);
+	
 	public ChunkListener(QuickShop plugin){
 		this.plugin = plugin;
 	}
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onChunkLoad(ChunkLoadEvent e){
+		//Testing
 		Chunk c = e.getChunk();
+		if(plugin.getShops() == null) return;
+		
+		List<Shop> shops = new ArrayList<Shop>(5);
+		
 		for(Shop shop : plugin.getShops().values()){
-			if(shop.getLocation().getChunk().equals(c)){
+			if(shop.getLocation().getChunk().isLoaded() && shop.getLocation().getChunk().equals(c)){
+				shops.add(shop);
+				
 				DisplayItem disItem = shop.getDisplayItem();
 				disItem.removeDupe();
 				disItem.remove();
 				disItem.spawn();
-				plugin.getLogger().info("Chunk loading spawning item: " + disItem.getItem().getItemStack().getType());
+				plugin.debug("Chunk loading spawning item: " + disItem.getItem().getItemStack().getType());
 			}
 		}
+		this.chunkMap.put(c, shops);
 	}
 	
 	@EventHandler(priority = EventPriority.LOW)
 	public void onChunkUnload(ChunkUnloadEvent e){
 		Chunk c = e.getChunk();
+		
+		List<Shop> shops = this.chunkMap.get(c);
+		
+		for(Shop shop : shops){
+			DisplayItem disItem = shop.getDisplayItem();
+			disItem.removeDupe();
+			disItem.remove();
+			plugin.debug("Chunk loading spawning item: " + disItem.getItem().getItemStack().getType());
+		}
+		/*
+		if(plugin.getShops() == null) return;
 		for(Shop shop : plugin.getShops().values()){
 			if(shop.getLocation().getChunk().equals(c)){
 				DisplayItem disItem = shop.getDisplayItem();
 				disItem.removeDupe();
 				disItem.remove();
 				disItem.spawn();
-				plugin.getLogger().info("Chunk loading spawning item: " + disItem.getItem().getItemStack().getType());
+				
 			}
-		}
+		}*/
 	}
 }
