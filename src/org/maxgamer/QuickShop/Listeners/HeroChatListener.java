f6dd0e58c51dad028f72bb72f57e4788feb638d2
Netherfoam <i.got.burnt@gmail.com>
2012-10-02 17:13:32 +1000

    Unused import

--- src/org/maxgamer/QuickShop/Listeners/HeroChatListener.java
+++ src/org/maxgamer/QuickShop/Listeners/HeroChatListener.java
@@ -3,7 +3,6 @@ package org.maxgamer.QuickShop.Listeners;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.maxgamer.QuickShop.QuickShop;
 
 import com.dthielke.herochat.ChannelChatEvent;
