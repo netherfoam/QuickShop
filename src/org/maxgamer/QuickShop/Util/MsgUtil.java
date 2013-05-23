5014a64deb1b3111f4b9f5fc153a4e1d7b0397bb
KittyTristy <kittytristy@gmail.com>
2013-05-23 12:54:06 -0700

    Showing tax in sendSellSuccess is now optional

--- src/org/maxgamer/QuickShop/Util/MsgUtil.java
+++ src/org/maxgamer/QuickShop/Util/MsgUtil.java
@@ -262,9 +262,14 @@ public class MsgUtil{
 		p.sendMessage(ChatColor.DARK_PURPLE + "+---------------------------------------------------+");
 		p.sendMessage(ChatColor.DARK_PURPLE + "| " + MsgUtil.getMessage("menu.successfully-sold"));
 		p.sendMessage(ChatColor.DARK_PURPLE + "| " + MsgUtil.getMessage("menu.item-name-and-price", ""+amount, shop.getDataName(), Util.format((amount * shop.getPrice()))));
-		double tax = plugin.getConfig().getDouble("tax");
-		double total = amount * shop.getPrice();
-		p.sendMessage(ChatColor.DARK_PURPLE + "| " + MsgUtil.getMessage("menu.sell-tax", ""+Util.format((tax * total))));
+				
+		if(plugin.getConfig().getBoolean("show-tax")) {
+			double tax = plugin.getConfig().getDouble("tax");
+			double total = amount * shop.getPrice();
+			if (tax != 0) {
+			p.sendMessage(ChatColor.DARK_PURPLE + "| " + MsgUtil.getMessage("menu.sell-tax", ""+Util.format((tax * total))));
+			}
+		}
 		
 		Map<Enchantment, Integer> enchs = shop.getItem().getItemMeta().getEnchants();
 		if(enchs != null && !enchs.isEmpty()){
